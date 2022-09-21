5978d52632521a67b1e50f90c25230a183e5331f
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-09-21 11:41:13 +0700

    Add files via upload

---
+++ Sesi 1/TugasSesi1No2.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Restu
+ */
+public class TugasSesi1No2 {
+    public static void main(String[] args) {
+        String belajar = JOptionPane.showInputDialog(null,"Anda sedang belajar apa ?");
+        JOptionPane.showMessageDialog(null, "Belajar "+belajar+" sangat mudah");
+    }
+}
